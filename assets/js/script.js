eb44f54e2b12b2ba18841f533b00fb0fbbcabe6c
Dev1l-princess <Dev1l.princess@yandex.ru>
2021-10-20 14:20:38 +0300

    save

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,15 @@
 const input = document.querySelector('#todo-input')
 const btnAdd = document.querySelector('#todo-add')
-const todoList = document.querySelector('.todo__list')
-const todoRemove = document.querySelectorAll('.todo__remove-button')
+const todoList = document.querySelector('#todo__list')
 
 btnAdd.addEventListener('click', () => {
   const value = input.value
-
   if (value !== '') 
-    todoList.innerHTML += `
-      <li class="todo__item">
-        <input type="checkbox" class="todo__checkbox">
-        <span> ${value} </span>
-        <button class="todo__remove-button">X</button>
-      </li>
-    `
-
+  sendName(value)
   input.value = ''
-
-  const todoChecks = document.querySelectorAll('.todo__checkbox')
-  todoChecks.forEach(input => 
-    input.addEventListener('click', e => completeCheck(e))
-  )
 })
+
+fetchTodo()
 
 function completeCheck(e) {
   const span = e.target.nextElementSibling
@@ -30,10 +18,66 @@ function completeCheck(e) {
 }
 
 todoList.addEventListener('click', e => {
-  if (e.target.classList.contains('todo__remove-button')) {
-    const li = e.target.closest('li')
-    li.remove()
-  } else if(e.target.classList.contains('todo__checkbox')) {
-    console.log('check');
+  const id = e.target.closest('li')?.dataset.id
+  const tag = e.target.tagName
+  const checked = e.target.checked;
+  if (id && tag === 'INPUT') {
+    checkTodo(id, checked)
+  }
+
+  if (id && tag === 'BUTTON') {
+    e.target.closest('li').remove()
+    removeTodo(id)
   }
 })
+
+function fetchTodo() {
+  fetch('./assets/php/todo.php')
+    .then(res => res.json())
+    .then(res => {
+      todoList.innerHTML = ''
+      res?.forEach(todo => {
+        let checked
+        if (todo.checked == 1) {
+          checked="checked"
+        }
+        todoList.innerHTML += `
+          <li class="todo__item" data-id='${todo.id}'>
+            <input type="checkbox" class="todo__checkbox" ${checked}>
+            <span> ${todo.text} </span>
+            <button class="todo__remove-button">X</button>
+          </li>
+        `
+      })
+    })
+}
+
+function sendName(todo) {
+  fetch('./assets/php/todo.php', {
+    method: 'POST',
+    body: JSON.stringify({
+      todo: todo
+    })
+  }).then(res => fetchTodo())
+}
+
+function checkTodo(id, checked) {
+  fetch('./assets/php/todo.php', {
+    method: 'PUT',
+    body: JSON.stringify({
+      id: id,
+      checked: checked
+    })
+  })
+}
+
+function removeTodo(id) {
+  fetch('./assets/php/todo.php', {
+    method: 'DELETE',
+    body: JSON.stringify({
+      id: id,
+    })
+  })
+}
+
+
